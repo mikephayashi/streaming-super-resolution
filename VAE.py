d465bbffebad618833e2975df5f2fd871c7c6db2
Michael Hayashi <mikephayashi@gmail.com>
2020-05-22 13:28:13 -0700

    update

--- VAE.py
+++ VAE.py
@@ -140,15 +140,15 @@ class VAE(nn.Module):
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 3, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 3, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2,     nc, 4, 3, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2,     nc, 4, 2, 1, bias=False),
             # nn.BatchNorm2d(ngf),
             # nn.ReLU(True),
             # state size. (ngf) x 32 x 32
