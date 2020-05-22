db1f3bac9d7cd60541c7c2bfaac044d31973c6ad
Michael Hayashi <mikephayashi@gmail.com>
2020-05-22 12:49:21 -0700

    update

--- VAE.py
+++ VAE.py
@@ -9,9 +9,11 @@ from torch.autograd import Variable
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
+
 
 class Break(nn.Module):
 
@@ -22,6 +24,7 @@ class Break(nn.Module):
 class UnFlatten(nn.Module):
     def forward(self, input, size=9216):
         return input.view(input.size(0), size, 1, 1)
+
 
 class VAE(nn.Module):
     def __init__(self, image_channels=3, h_dim=9216, z_dim=32):
@@ -34,15 +37,14 @@ class VAE(nn.Module):
             nn.Conv2d(64, 128, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(128, 256, kernel_size=4, stride=2),
-            Break(),
             nn.ReLU(),
             Flatten()
         )
-        
+
         self.fc1 = nn.Linear(h_dim, z_dim)
         self.fc2 = nn.Linear(h_dim, z_dim)
         self.fc3 = nn.Linear(z_dim, h_dim)
-        
+
         self.decoder = nn.Sequential(
             UnFlatten(),
             nn.ConvTranspose2d(h_dim, 128, kernel_size=4, stride=3),
@@ -54,24 +56,39 @@ class VAE(nn.Module):
             nn.ConvTranspose2d(32, image_channels, kernel_size=5, stride=3),
             nn.Sigmoid(),
         )
-        
+
     def reparameterize(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
         # return torch.normal(mu, std)
         esp = torch.randn(*mu.size()).to(device)
         z = mu + std * esp
         return z
-    
+
     def bottleneck(self, h):
         mu, logvar = self.fc1(h), self.fc2(h)
         z = self.reparameterize(mu, logvar)
         return z, mu, logvar
-        
+
     def representation(self, x):
         return self.bottleneck(self.encoder(x))[0]
 
     def forward(self, x, iteration):
-        self.iteration = iteration
+        if iteration == 13:
+            self.encoder = nn.Sequential(
+                nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
+                Break(),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                Break(),
+                nn.ReLU(),
+                nn.Conv2d(64, 128, kernel_size=4, stride=2),
+                Break(),
+                nn.ReLU(),
+                nn.Conv2d(128, 256, kernel_size=4, stride=2),
+                Break(),
+                nn.ReLU(),
+                Flatten()
+            )
         h = self.encoder(x)
         z, mu, logvar = self.bottleneck(h)
         z = self.fc3(z)
