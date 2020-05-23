825b6a17041792fdfc9071a905ea267fcc2ce4d8
Michael Hayashi <mikephayashi@gmail.com>
2020-05-22 21:52:11 -0700

    update

--- models/VAE.py
+++ models/VAE.py
@@ -38,9 +38,9 @@ class VAE(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=4),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=7, stride=3),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(128, 256, kernel_size=2, stride=2),
             nn.ReLU(),
@@ -53,13 +53,13 @@ class VAE(nn.Module):
 
         self.decoder = nn.Sequential(
             UnFlatten(),
-            nn.ConvTranspose2d(h_dim, 128, kernel_size=5, stride=5),
+            nn.ConvTranspose2d(h_dim, 128, kernel_size=2, stride=2),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=5),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=4),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=5, stride=5),
+            nn.ConvTranspose2d(64, 32, kernel_size=7, stride=3),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, image_channels, kernel_size=3, stride=3),
+            nn.ConvTranspose2d(32, image_channels, kernel_size=2, stride=2),
             nn.Sigmoid(),
         )
 
@@ -83,6 +83,57 @@ class VAE(nn.Module):
         z, mu, logvar = self.bottleneck(h)
         z = self.fc3(z)
         return self.decoder(z), mu, logvar
+# class VAE(nn.Module):
+#     def __init__(self, image_channels=3, h_dim=256, z_dim=32):
+#         super(VAE, self).__init__()
+#         self.encoder = nn.Sequential(
+#             nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
+#             nn.ReLU(),
+#             nn.Conv2d(32, 64, kernel_size=4, stride=4),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 128, kernel_size=7, stride=3),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 256, kernel_size=2, stride=2),
+#             nn.ReLU(),
+#             Flatten()
+#         )
+
+#         self.fc1 = nn.Linear(h_dim, z_dim)
+#         self.fc2 = nn.Linear(h_dim, z_dim)
+#         self.fc3 = nn.Linear(z_dim, h_dim)
+
+#         self.decoder = nn.Sequential(
+#             UnFlatten(),
+#             nn.ConvTranspose2d(h_dim, 128, kernel_size=5, stride=5),
+#             nn.ReLU(),
+#             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=5),
+#             nn.ReLU(),
+#             nn.ConvTranspose2d(64, 32, kernel_size=5, stride=5),
+#             nn.ReLU(),
+#             nn.ConvTranspose2d(32, image_channels, kernel_size=3, stride=3),
+#             nn.Sigmoid(),
+#         )
+
+#     def reparameterize(self, mu, logvar):
+#         std = logvar.mul(0.5).exp_()
+#         # return torch.normal(mu, std)
+#         esp = torch.randn(*mu.size()).to(device)
+#         z = mu + std * esp
+#         return z
+
+#     def bottleneck(self, h):
+#         mu, logvar = self.fc1(h), self.fc2(h)
+#         z = self.reparameterize(mu, logvar)
+#         return z, mu, logvar
+
+#     def representation(self, x):
+#         return self.bottleneck(self.encoder(x))[0]
+
+#     def forward(self, x):
+#         h = self.encoder(x)
+#         z, mu, logvar = self.bottleneck(h)
+#         z = self.fc3(z)
+#         return self.decoder(z), mu, logvar
 
 """
 360
