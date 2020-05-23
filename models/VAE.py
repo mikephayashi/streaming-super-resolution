c62f218d9a4757ab8bcaabb1190a6fc6e925d6a2
Michael Hayashi <mikephayashi@gmail.com>
2020-05-22 19:10:30 -0700

    update

--- models/VAE.py
+++ models/VAE.py
@@ -50,9 +50,9 @@ class VAE(nn.Module):
 
         self.decoder = nn.Sequential(
             UnFlatten(),
-            nn.ConvTranspose2d(h_dim, 128, kernel_size=4, stride=5),
+            nn.ConvTranspose2d(h_dim, 128, kernel_size=4, stride=4),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=5),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=4),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, kernel_size=6, stride=5),
             nn.ReLU(),
