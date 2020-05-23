fbb6ce85f1ad0d92ad5c34aaeffda887d733b479
Michael Hayashi <mikephayashi@gmail.com>
2020-05-22 20:07:44 -0700

    update

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ MODEL_NAME = None
 argv = sys.argv[1:]
 
 try:
-    opt, args = getopt.getopt(argv, ":n", "name=")
+    opts, args = getopt.getopt(argv, ":n", "name=")
 except getopt.GetoptError:
     print("Add model name")
 
